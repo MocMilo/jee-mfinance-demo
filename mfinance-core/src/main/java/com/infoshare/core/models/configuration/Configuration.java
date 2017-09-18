161f47a9e9f914c74bc445db40871089584f395a
MocMilo <mmociewicz@gmail.com>
2017-09-18 23:18:45 +0200

    Configuration unnecessary setters removed.

--- mfinance-core/src/main/java/com/infoshare/core/models/configuration/Configuration.java
+++ mfinance-core/src/main/java/com/infoshare/core/models/configuration/Configuration.java
@@ -27,71 +27,37 @@ public class Configuration {
         return fundFolderPath;
     }
 
-    public void setFundFolderPath(FundFolderPath fundFolderPath) {
-        this.fundFolderPath = fundFolderPath;
-    }
-
     public CurrencyFolderPath getCurrencyFolderPath() {
         return currencyFolderPath;
-    }
-
-    public void setCurrencyFolderPath(CurrencyFolderPath currencyFolderPath) {
-        this.currencyFolderPath = currencyFolderPath;
     }
 
     public FundBackupFolderPath getFundBackupFolderPath() {
         return fundBackupFolderPath;
     }
 
-    public void setFundBackupFolderPath(FundBackupFolderPath fundBackupFolderPath) {
-        this.fundBackupFolderPath = fundBackupFolderPath;
-    }
-
     public CurrencyBackupFolderPath getCurrencyBackupFolderPath() {
         return currencyBackupFolderPath;
-    }
-
-    public void setCurrencyBackupFolderPath(CurrencyBackupFolderPath currencyBackupFolderPath) {
-        this.currencyBackupFolderPath = currencyBackupFolderPath;
     }
 
     public FundUrl getFundUrl() {
         return fundUrl;
     }
 
-    public void setFundUrl(FundUrl fundUrl) {
-        this.fundUrl = fundUrl;
-    }
-
     public CurrencyUrl getCurrencyUrl() {
         return currencyUrl;
-    }
-
-    public void setCurrencyUrl(CurrencyUrl currencyUrl) {
-        this.currencyUrl = currencyUrl;
     }
 
     public String getExternalResourceFilePath() {
         return externalResourceFilePath;
     }
 
-    public void setExternalResourceFilePath(String externalResourceFilePath) {
-        this.externalResourceFilePath = externalResourceFilePath;
-    }
-
     public List<FilePath> getFundFilePaths() {
         return fundFilePaths;
-    }
-
-    public void setFundFilePaths(List<FilePath> fundFilePaths) {
-        this.fundFilePaths = fundFilePaths;
     }
 
     public List<FilePath> getCurrencyFilePaths() {
         return currencyFilePaths;
     }
 
-    public void setCurrencyFilePaths(List<FilePath> currencyFilePaths) {
-        this.currencyFilePaths = currencyFilePaths;
-    }
+
 }
