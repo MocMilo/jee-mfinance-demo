7cc388f1e1575c7b5e94ebdd10c42571eeb78ad0
MocMilo <mmociewicz@gmail.com>
2017-09-19 20:19:28 +0200

    Configuration adjustment

--- mfinance-web/src/main/java/com/infoshare/web/models/Configuration.java
+++ mfinance-web/src/main/java/com/infoshare/web/models/Configuration.java
@@ -1,4 +1,4 @@
-package com.infoshare.core.models.configuration;
+package com.infoshare.web.models;
 
 import com.infoshare.core.file.path.*;
 import com.infoshare.core.file.url.CurrencyUrl;
@@ -9,54 +9,9 @@ import java.util.List;
 
 public class Configuration {
 
-    private FundFolderPath fundFolderPath;
-    private CurrencyFolderPath currencyFolderPath;
-
-    private FundBackupFolderPath fundBackupFolderPath;
-    private CurrencyBackupFolderPath currencyBackupFolderPath;
-
-    private FundUrl fundUrl;
-    private CurrencyUrl currencyUrl;
-
     private String externalResourceFilePath;
-
-    private List<FilePath> fundFilePaths = new ArrayList<>();
-    private List<FilePath> currencyFilePaths = new ArrayList<>();
-
-    public FundFolderPath getFundFolderPath() {
-        return fundFolderPath;
-    }
-
-    public CurrencyFolderPath getCurrencyFolderPath() {
-        return currencyFolderPath;
-    }
-
-    public FundBackupFolderPath getFundBackupFolderPath() {
-        return fundBackupFolderPath;
-    }
-
-    public CurrencyBackupFolderPath getCurrencyBackupFolderPath() {
-        return currencyBackupFolderPath;
-    }
-
-    public FundUrl getFundUrl() {
-        return fundUrl;
-    }
-
-    public CurrencyUrl getCurrencyUrl() {
-        return currencyUrl;
-    }
-
     public String getExternalResourceFilePath() {
         return externalResourceFilePath;
-    }
-
-    public List<FilePath> getFundFilePaths() {
-        return fundFilePaths;
-    }
-
-    public List<FilePath> getCurrencyFilePaths() {
-        return currencyFilePaths;
     }
 
 
