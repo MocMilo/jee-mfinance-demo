3c1b63c4bc823b207433e22851b42a7db65021d4
MocMilo <mmociewicz@gmail.com>
2017-09-24 20:10:33 +0200

    mfinance-core exceptions handling in mfinance-cli implemented

--- mfinance-cli/src/main/java/com/infoshare/controller/MainController.java
+++ mfinance-cli/src/main/java/com/infoshare/controller/MainController.java
@@ -1,7 +1,6 @@
 package com.infoshare.controller;
 
 import com.infoshare.controller.analyzers.strategies.AnalysisContext;
-import com.infoshare.controller.analyzers.strategies.IVRAnalyzerStrategy;
 import com.infoshare.controller.validators.strategies.ValidationContext;
 import com.infoshare.core.models.exceptions.NoDataForCriteria;
 import com.infoshare.model.validationResults.AnalysisValidationResult;
@@ -13,20 +12,25 @@ public class MainController {
     private AnalysisValidationResult result;
     private WelcomeMessageComposer composer = new WelcomeMessageComposer();
 
-    public void initialize(String[] args) throws NoDataForCriteria {
+    public void initialize(String[] args) {
         composer.printWelcomeMessage(args);
         doExecute(args);
     }
 
-    private void doExecute(String[] args) throws NoDataForCriteria {
-        if (args.length > 0) {
-            result = ValidationContext.doValidate(args);
-            if(!result.isValid()) {
-                System.out.print(result.getErrMessage());
+    private void doExecute(String[] args) {
+        try {
+            if (args.length > 0) {
+                result = ValidationContext.doValidate(args);
+                if (!result.isValid()) {
+                    System.out.print(result.getErrMessage());
+                } else {
+                    AnalysisContext.doAnalyze(args);
+                }
             }
-            else {
-                AnalysisContext.doAnalyze(args);
-            }
+        } catch (NoDataForCriteria e) {
+            System.out.printf(e.getMessage() + "\n");
+        } catch (IllegalArgumentException e) {
+            System.out.printf(e.getMessage() + "\n");
         }
     }
 }
