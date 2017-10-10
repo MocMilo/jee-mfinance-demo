3f212b37187740f9a9aa1102614c5e557a71ce38
MocMilo <mmociewicz@gmail.com>
2017-10-10 05:20:21 +0200

    Quotation cleanup

--- mfinance-core/src/main/java/com/infoshare/mfinance/core/models/bossa/Quotation.java
+++ mfinance-core/src/main/java/com/infoshare/mfinance/core/models/bossa/Quotation.java
@@ -11,21 +11,11 @@ public class Quotation implements Comparable<Quotation> {
     private BigDecimal close;
     private BigDecimal deltaClose;
 
-/*    private BigDecimal high;
-    private BigDecimal low;
-    private BigDecimal volume;*/
-
-
     public Quotation(String name, LocalDate date, BigDecimal close) {
         this.name = name;
         this.date = date;
         this.close = close;
-       // this.open = open;
-/*        this.high = high;
-        this.low = low;*/
 
-        // this.deltaClose =deltaClose;
-     /*   this.volume = volume;*/
     }
 
     public String getName() {
@@ -44,7 +34,6 @@ public class Quotation implements Comparable<Quotation> {
         this.date = date;
     }
 
-
     public BigDecimal getClose() {
         return close;
     }
@@ -61,30 +50,6 @@ public class Quotation implements Comparable<Quotation> {
         this.open = open;
     }
 
-    /*public BigDecimal getHigh() {
-        return high;
-    }
-
-    public void setHigh(BigDecimal high) {
-        this.high = high;
-    }
-
-    public BigDecimal getLow() {
-        return low;
-    }
-
-    public void setLow(BigDecimal low) {
-        this.low = low;
-    }
-
-    public BigDecimal getVolume() {
-        return volume;
-    }
-
-    public void setVolume(BigDecimal volume) {
-        this.volume = volume;
-    }
-*/
     public BigDecimal getDeltaClose() {
         return deltaClose;
     }
@@ -96,15 +61,10 @@ public class Quotation implements Comparable<Quotation> {
     @Override
     public String toString() {
         return "Quotation [name=" + name + ", date=" + date +
-              //  ", open=" + open +
-              //  ", high=" + high +
-              //  ", low=" +low+
                 ", " + "close=" +close+
-               // " , volume=" +volume+
                 ", deltaClose="+deltaClose+"%] \n";
 
     }
-
 
     @Override
     public int compareTo(Quotation o) {
@@ -112,5 +72,4 @@ public class Quotation implements Comparable<Quotation> {
             return 0;
         return getDate().compareTo(o.getDate());
     }
-
 }
