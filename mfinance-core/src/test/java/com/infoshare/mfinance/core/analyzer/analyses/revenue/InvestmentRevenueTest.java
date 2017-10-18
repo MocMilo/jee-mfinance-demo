931d6a20ab2575f68710a000c912c92cce407746
MocMilo <mmociewicz@gmail.com>
2017-10-18 08:10:57 +0200

    [core] InvestmentRevenueAnalysis test refactored

--- mfinance-core/src/test/java/com/infoshare/mfinance/core/analyzer/analyses/revenue/InvestmentRevenueTest.java
+++ mfinance-core/src/test/java/com/infoshare/mfinance/core/analyzer/analyses/revenue/InvestmentRevenueTest.java
@@ -1,75 +1,88 @@
 package com.infoshare.mfinance.core.analyzer.analyses.revenue;
 
-import com.infoshare.mfinance.core.builders.DataContainerBuilder;
 import com.infoshare.mfinance.core.models.analyses.criteria.InvestmentRevenueCriteria;
 import com.infoshare.mfinance.core.models.analyses.results.InvestmentRevenueResult;
+import com.infoshare.mfinance.core.models.bossa.Currency;
 import com.infoshare.mfinance.core.models.bossa.DataContainer;
+import com.infoshare.mfinance.core.models.bossa.Investment;
+import com.infoshare.mfinance.core.models.bossa.Quotation;
 import com.infoshare.mfinance.core.models.exceptions.NoDataForCriteria;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.core.IsNot.not;
 
 
 public class InvestmentRevenueTest {
 
-    private static final DataContainerBuilder MAIN_CONTAINER_BUILDER = new DataContainerBuilder();
-    private static DataContainer container;
+    private DataContainer container = new DataContainer();
+    private List<Investment> investments = new ArrayList<>();
+    private List<Quotation> quotations = new ArrayList<>();
+    private Quotation quotation1;
+    private Quotation quotation2;
+    private Quotation quotation3;
+    private Currency currency;
+
     private final BigDecimal capital = new BigDecimal(10000.00);
     private final DateTimeFormatter formatter = DateTimeFormatter.BASIC_ISO_DATE;
     private final LocalDate BUY_DATE = LocalDate.parse("20090910", formatter);
+    private final LocalDate SELL_DATE = LocalDate.parse("20090912", formatter);
     private final String InvestmentName = "CHF";
 
-    @BeforeClass
-    public static void init() {
-        container = MAIN_CONTAINER_BUILDER.getDataContainer();
+    @Before
+    public void init() {
+        quotation1 = new Quotation("CHF", LocalDate.parse("20090910", formatter), new BigDecimal(4.50));
+        quotation2 = new Quotation("CHF", LocalDate.parse("20090911", formatter), new BigDecimal(4.60));
+        quotation3 = new Quotation("CHF", LocalDate.parse("20090912", formatter), new BigDecimal(4.70));
+
+        quotations.add(quotation1);
+        quotations.add(quotation2);
+        quotations.add(quotation3);
+
+        currency = new Currency("CHF", quotations);
+        investments.add(currency);
+        container.setInvestments(investments);
+        container.setCurrenciesCount(1);
     }
 
     @Test
     public void getInvestmentsFromMainContainer() {
-
-
         int investments = container.getInvestments().size();
-
         assertThat(investments, not(equalTo(nullValue())));
+    }
 
-        System.out.println("number of investments: " + investments);
+    @Test
+    public void shouldGetAnalysisResult() throws NoDataForCriteria{
+
+        InvestmentRevenueCriteria criteria = new InvestmentRevenueCriteria(capital, BUY_DATE, SELL_DATE, InvestmentName);
+        InvestmentRevenueResult result = new InvestmentRevenue(container, criteria).getResult();
+
+        assertThat(result.getCapitalRevenueValue(), not(equalTo(nullValue())));
+        assertThat(result.getCapitalRevenueDeltaPercentValue(), not(equalTo(nullValue())));
     }
 
     @Test(expected = NoDataForCriteria.class)
     public void testGetResultWhenUserInputOutOfRange() throws Exception {
+
         LocalDate SELL_DATE = LocalDate.parse("20140104", formatter);
-
-        // example analyses usage
-        InvestmentRevenueCriteria input = new InvestmentRevenueCriteria(capital, BUY_DATE, SELL_DATE, InvestmentName);
-        InvestmentRevenueResult ir = new InvestmentRevenue(container, input).getResult();
-
-        // analyses input
-        assertThat(input.getBuyDate(), not(equalTo(nullValue())));
-        assertThat(input.getSellDate(), not(equalTo(nullValue())));
-
-        // analysis results
-        assertThat(ir.getCapitalRevenueValue(), is(equalTo(nullValue())));
-        assertThat(ir.getCapitalRevenueDeltaPercentValue(), is(equalTo(nullValue())));
+        InvestmentRevenueCriteria criteria = new InvestmentRevenueCriteria(capital, BUY_DATE, SELL_DATE, InvestmentName);
+        new InvestmentRevenue(container, criteria).getResult();
     }
 
     @Test(expected = NoDataForCriteria.class)
     public void testGetResultWhenMissingQuotations() throws Exception {
-        LocalDate SELL_DATE = LocalDate.parse("20170330", formatter);
 
-        // ESSENTIAL: removing quotations
         container.getInvestments().forEach(x -> x.setQuotations(null));
-
-        // example analyses usage
-        InvestmentRevenueCriteria input = new InvestmentRevenueCriteria(capital, BUY_DATE, SELL_DATE, InvestmentName);
-        InvestmentRevenueResult ir = new InvestmentRevenue(container, input).getResult();
+        InvestmentRevenueCriteria criteria = new InvestmentRevenueCriteria(capital, BUY_DATE, SELL_DATE, InvestmentName);
+        new InvestmentRevenue(container, criteria).getResult();
     }
 }
